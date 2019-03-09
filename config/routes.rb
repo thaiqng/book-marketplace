7d087ca4d3a5e39b3a01569d45de771b1511db96
thai <nguyenquangthai.tm@gmail.com>
2019-03-09 20:55:10 +0700

    iterG/form-checkout-AtomFeed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
-  get 'store/index', as: 'store_index'
+  root 'store#index', as: 'store_index'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
 end
